2b334c22ec3e606fca7c596953813ac3c565429b
kjacome24 <kevin.arturo.jacome.duque@gmail.com>
2025-09-08 22:21:49 -0400

    Kevin J 08/09/2025 21:05

---
+++ x_08_09_2025/Flormania/script.js
@@ -0,0 +1,18 @@
+console.log("Js is up and running");
+
+
+function comprar(input){
+    input.remove();
+}
+
+function iniciar_sesion(){
+    alert("Bienvenido a la tienda de flores")
+}
+
+function buquets(input){
+    input.innerText = "Envia buquests";
+}
+
+function flores(input){
+    input.innerText = "Envia Flores";
+}
