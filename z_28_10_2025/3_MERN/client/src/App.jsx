9b533e96857a6fb170ef5cd221bc71888a6e95c4
kjacome24 <kevin.arturo.jacome.duque@gmail.com>
2025-10-28 23:09:53 -0400

    Kevin J 28/10/2025 23:09

---
+++ z_28_10_2025/3_MERN/client/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import {Routes, Route, Link, NavLink} from 'react-router-dom'
+import './App.css'
+import SongsApi from './components/SongsApi'
+import ListaCanciones from './components/ListaCanciones'
+import OneSong from './components/OneSong'
+import NotFound from './components/NotFound'
+import FormSongs from './components/FormSongs'
+
+function App() {
+  const [listaSongs, setListasSongs] = useState([])
+
+  return (
+    <>
+      <h1>This is our first MERN Stack app</h1>
+      < SongsApi setListasSongs={setListasSongs}/> 
+      < NavLink to={'/canciones'}>Listado Completo</NavLink> | < NavLink to={'/canciones/new'}>Agregar cancion </NavLink>
+      < Routes>
+        <Route path='/' element={<div><p>This is the home</p></div>}/>
+        <Route path='/canciones' element={< ListaCanciones listaSongs={listaSongs}/>}/>
+        <Route path='/canciones/:id' element={<OneSong listaSongs={listaSongs}  setListasSongs={setListasSongs}/> }/>
+        <Route path='/canciones/new' element={< FormSongs listaSongs={listaSongs}  setListasSongs={setListasSongs}/>}/>
+        <Route path='*' element={< NotFound/>}/>
+      </Routes>
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ z_28_10_2025/3_MERN/client/src/components/OneSong.jsx
@@ -0,0 +1,54 @@
+import { useNavigate, useParams } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import NotFound from "./NotFound";
+
+
+const OneSong = ({listaSongs, setListasSongs})=> {
+    const [person, setPerson] = useState({})
+    const [error, setError] = useState('')
+    const {id} = useParams();
+    const URL = `http://localhost:8000/api/canciones/${id}`
+    const navigate = useNavigate();
+    const getData = ()=>{
+        axios(URL).then(response => 
+            setPerson(response.data)
+        ).catch(
+            e=> setError(e)
+        )
+    }
+
+    useEffect(()=>{
+        getData()
+    },[])
+
+    if(error){
+        return <NotFound/>
+    }
+
+
+    const deleteOne =()=>{
+        axios.delete(URL).then(
+            response => {
+
+                setListasSongs(listaSongs.filter(song => song._id != id ))
+                navigate('/canciones')
+            } 
+        ).catch(
+            e => console.log(e)
+        )
+    }
+
+    return(
+        <div>
+                <h2>Detalle de cancion</h2>
+                <p>Title: {person.title}</p>
+                <p>Artist: {person.artist}</p>
+                <p>Lanzamiento: {person.yearOfRealease}</p>
+                <p>Genero: {person.genre}</p>
+                <button onClick={deleteOne}>Eliminar</button>
+        </div>
+    )
+}
+
+export default OneSong;
